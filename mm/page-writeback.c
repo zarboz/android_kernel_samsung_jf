8622c8bb018477d5931ef812a7da7cb8b4bc609a
Dennis Rassmann <showp1984@gmail.com>
2012-12-19 11:21:34 +0100

    cpuinit: get rid of __cpuinit, fourth regexp
    This commit is the result of:
    
    find -name "*.c" | xargs sed -i "s/ __cpuinit$//g"
    
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    
    Change-Id: Ibad8f0019463f180fc131c2d855396134bbc7baf
    Signed-off-by: Zarboz <zarboz@gmail.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -1587,9 +1587,10 @@ void writeback_set_ratelimit(void)
 		ratelimit_pages = 16;
 }
 
-static int __cpuinit
+static int 
 ratelimit_handler(struct notifier_block *self, unsigned long action,
 		  void *hcpu)
+
 {
 
 	switch (action & ~CPU_TASKS_FROZEN) {
