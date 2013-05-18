da5d86159a5410ffd3c8c017d7c36ee0b7441627
Matt Filetto <matt.filetto@gmail.com>
2013-05-17 21:59:02 -0700

    mach-msm/cpufreq: add a define for SEC_DVFS max OC freq
    Change-Id: I7c56dd9901b50f015482d4be1169e0ed0dbe838d

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -32,6 +32,8 @@
 
 #include "acpuclock.h"
 
+#define SEC_DVFS 2106000
+
 struct cpufreq_work_struct {
 	struct work_struct work;
 	struct cpufreq_policy *policy;
@@ -65,7 +67,7 @@ static DEFINE_PER_CPU(struct cpu_freq, cpu_freq_info);
 #ifdef CONFIG_SEC_DVFS_BOOSTER
 static unsigned int upper_limit_freq = 1566000;
 #else
-static unsigned int upper_limit_freq = 2106000;
+static unsigned int upper_limit_freq = SEC_DVFS;
 #endif
 static unsigned int lower_limit_freq;
 static unsigned int cpuinfo_max_freq;
