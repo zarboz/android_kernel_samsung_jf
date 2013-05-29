fdacafb02e54bcc73219c48d71512616dc118d73
ktoonsez <ktoonsez@gmail.com>
2013-05-28 23:19:31 -0700

    Fix zero issue for GPU Max mhz for screen OFF

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -695,7 +695,7 @@ static ssize_t store_screen_off_GPU_mhz(struct cpufreq_policy *policy,
 	ret = sscanf(buf, "%ld", &value);
 	if (value > 500000000)
 		value = 500000000;
-	if (value < 128000000)
+	if (value < 128000000 && value != 0)
 		value = 128000000;
 	Lscreen_off_GPU_mhz = value;
 
@@ -2505,7 +2505,7 @@ static void cpufreq_gov_resume(void)
 	}
 	
 	//GPU Control
-	if (Lscreen_off_GPU_mhz != 0)
+	if (Lscreen_off_GPU_mhz > 0)
 		set_max_gpuclk_so(0);
 }
 
@@ -2555,7 +2555,7 @@ static void cpufreq_gov_suspend(void)
 	}
 	
 	//GPU Control
-	if (Lscreen_off_GPU_mhz != 0)
+	if (Lscreen_off_GPU_mhz > 0)
 		set_max_gpuclk_so(Lscreen_off_GPU_mhz);
 }
 
