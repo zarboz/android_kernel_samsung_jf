a4d6eba960cf832eff9c89c4f0199b6cde7e95c3
Shareef Ali <shareefalis@gmail.com>
2013-06-25 06:59:50 -0500

    upgrade to google sources
    Conflicts:
    	arch/arm/mach-msm/board-jf_cri.c
    	arch/arm/mach-msm/board-jf_dcm.c
    	arch/arm/mach-msm/devices-8064.c
    	arch/arm/mach-msm/hotplug.c
    	arch/arm/mach-msm/pm-8x60.c
    	drivers/gpu/ion/msm/msm_ion.c
    	drivers/input/keyboard/gpio_keys.c
    	drivers/media/video/msm/sensors/jc_v4l2.c
    	drivers/video/msm/mdp.c
    	drivers/video/msm/mdp.h
    	drivers/video/msm/mdp4.h
    	drivers/video/msm/mdp4_overlay.c
    	drivers/video/msm/mdp4_overlay_dsi_cmd.c
    	drivers/video/msm/mdp4_overlay_dsi_video.c
    	drivers/video/msm/mdp4_overlay_dtv.c
    	drivers/video/msm/vidc/common/vcd/vcd_power_sm.c
    Change-Id: Icac51ed5ab5e58e0ec1eeaf6c9d4306db6c613c3
    
    Conflicts:
    	fs/ecryptfs/keystore.c

--- arch/arm/mach-msm/board-jf_spr.c
+++ arch/arm/mach-msm/board-jf_spr.c
@@ -4395,7 +4395,11 @@ static struct gpio_keys_button gpio_keys_button[] = {
 		.active_low     = 1,
 		.type		= EV_KEY,
 		.wakeup		= 0,
+#ifdef CONFIG_SEC_FACTORY
+		.debounce_interval = 10,
+#else
 		.debounce_interval = 5,
+#endif
 	},
 	{
 		.code           = KEY_VOLUMEDOWN,
@@ -4404,7 +4408,11 @@ static struct gpio_keys_button gpio_keys_button[] = {
 		.active_low     = 1,
 		.type		= EV_KEY,
 		.wakeup		= 0,
+#ifdef CONFIG_SEC_FACTORY
+		.debounce_interval = 10,
+#else
 		.debounce_interval = 5,
+#endif
 	},
 	{
 		.code           = KEY_HOMEPAGE,
@@ -4413,7 +4421,11 @@ static struct gpio_keys_button gpio_keys_button[] = {
 		.active_low     = 1,
 		.type		= EV_KEY,
 		.wakeup		= 1,
+#ifdef CONFIG_SEC_FACTORY
+		.debounce_interval = 10,
+#else
 		.debounce_interval = 5,
+#endif
 	},
 };
 
