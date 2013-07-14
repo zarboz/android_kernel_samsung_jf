fda6bca18f65374c31bfde6905ebcea35ae11fd1
Matt Filetto <matt.filetto@gmail.com>
2013-07-13 23:48:36 -0700

    build-all-variants: add sprint to build
    Change-Id: I90ef62f5f1f1c66036997a13c8649ed2edd1d3d0

--- build-all-variants.sh
+++ build-all-variants.sh
@@ -13,8 +13,8 @@ echo "Make Mrproper"
 make mrproper
 ./build-vzw.sh
 echo "Make Mrproper"
-#make mrproper
-#./build-spr.sh
+make mrproper
+./build-spr.sh
 echo "build All Branches Complete"
 time_end_full=$(date +%s.%N)
 echo -e "${BLDYLW}Total time elapsed: ${TCTCLR}${TXTGRN}$(echo "($time_end_full - $time_start_full) / 60"|bc ) ${TXTYLW}minutes${TXTGRN} ($(echo "$time_end_full - $time_start_full"|bc ) ${TXTYLW}seconds) ${TXTCLR}"
